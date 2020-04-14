f4dd5c545adcafd2748fc882c7c82a26b031a35f
Egor Vasyakin <evasyakin@mail.ru>
2020-04-14 18:40:20 +0700

    Create db.example.php

---
+++ config/db.example.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Пример конфига php.
+ * Реальный конфиг должен называться db.php
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 13 Apr 2020
+ */
+return [
+    // 'driver' => 'mysql', // драйвер, по умолчанию mysql
+    // 'host' => 'localhost', // хост, по умолчанию localhost
+    // 'charset' => 'utf8', // кодировка,  по умолчанию utf8
+    'username' => 'root', // имя пользователя
+    'password' => '1234', // пароль
+    'dbname' => 'test'; // имя базы данных
+];
