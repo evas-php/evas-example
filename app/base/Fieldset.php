38962958728c164a08b985d14b06cc76d0957f64
Egor Vasyakin <evasyakin@mail.ru>
2020-04-15 23:16:01 +0700

    Create Fieldset.php

---
+++ app/base/Fieldset.php
@@ -0,0 +1,12 @@
+<?php
+namespace base;
+
+use Evas\Validate\Fieldset as EvasFieldset;
+
+/**
+ * Базовый валидатор набора полей.
+ */
+class Fieldset extends EvasFieldset
+{
+    // тут можно кастомизировать валидатор от evas-php
+}
