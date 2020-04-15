dd89d6dbb633b7a9b8e31c1cb8f802f81f372c8e
Egor Vasyakin <evasyakin@mail.ru>
2020-04-15 23:16:04 +0700

    Create Field.php

---
+++ app/base/Field.php
@@ -0,0 +1,12 @@
+<?php
+namespace base;
+
+use Evas\Validate\Field as EvasField;
+
+/**
+ * Базовый валидатор поля.
+ */
+class Field extends EvasField
+{
+    // тут можно кастомизировать валидатор от evas-php
+}
